0baaa7cab49f427630e565e40e4b841c82ea87d5
Sayed <sayedrkhalid.official@gmail.com>
2026-04-30 17:52:50 +0600

    feature: add navbar feature

---
+++ src/components/layout/Navbar/NavLink.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavLink = ({ href, label }) => {
+  const pathName = usePathname();
+
+  return (
+    <Link href={href} className={`${pathName === href && "text-blue-500"}`}>
+      {label}
+    </Link>
+  );
+};
+
+export default NavLink;
