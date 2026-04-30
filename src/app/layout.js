8bc54e16202100789f8b589b5bbcbf31b12be68d
Sayed <sayedrkhalid.official@gmail.com>
2026-04-30 18:09:53 +0600

    feature: add logo color

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,10 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body suppressHydrationWarning className="min-h-full flex flex-col">
+      <body
+        suppressHydrationWarning
+        className="min-h-full flex flex-col bg-gray-100"
+      >
         <Navbar />
         <main>{children}</main>
       </body>
